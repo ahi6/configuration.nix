0d21da48cf00297b0f9e4a448b675f66d2babb2b
ahi6 <60984726+ahi6@users.noreply.github.com>
2024-06-15 11:36:50 +0200

    Add note to davinci.nix
    I legit thought the app just wasn't working

--- modules/davinci.nix
+++ modules/davinci.nix
@@ -1,3 +1,7 @@
+# NOTE: DaVinci Resolve (Free edition) doesn't support support the H.264 codec on Linux
+# Most videos require being manually processed through ffmpeg first
+# example: ffmpeg -i input.mp4 -c:v dnxhd -b:v 120M -vf "scale=1920:1080,fps=24" -c:a pcm_s16le output.mov
+
 {
   pkgs,
   lib,
