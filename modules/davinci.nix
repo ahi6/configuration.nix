0fd45428977e0287cf5ce5f13697b954f4dd808c
ahi6 <60984726+ahi6@users.noreply.github.com>
2024-06-15 11:39:39 +0200

    Add another note, just in case

--- modules/davinci.nix
+++ modules/davinci.nix
@@ -2,6 +2,7 @@
 # Most videos require being manually processed through ffmpeg first
 # example: ffmpeg -i input.mp4 -c:v dnxhd -b:v 120M -vf "scale=1920:1080,fps=24" -c:a pcm_s16le output.mov
 
+# DaVinci Resolve doesn't create a desktop shortcut, run it via `davinci-resolve` in the terminal in the home directory
 {
   pkgs,
   lib,
