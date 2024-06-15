63cb6391b5c581453e0698a9a7a61ac7c83e80af
ahi6 <60984726+ahi6@users.noreply.github.com>
2024-06-15 11:44:39 +0200

    Formatting

--- modules/davinci.nix
+++ modules/davinci.nix
@@ -1,7 +1,6 @@
 # NOTE: DaVinci Resolve (Free edition) doesn't support support the H.264 codec on Linux
 # Most videos require being manually processed through ffmpeg first
 # example: ffmpeg -i input.mp4 -c:v dnxhd -b:v 120M -vf "scale=1920:1080,fps=24" -c:a pcm_s16le output.mov
-
 # DaVinci Resolve doesn't create a desktop shortcut, run it via `davinci-resolve` in the terminal in the home directory
 {
   pkgs,
