28472a6187e5a0cfbfc4eb1d5561fb708576d242
ahi6 <60984726+ahi6@users.noreply.github.com>
2024-10-14 18:08:34 +0200

    158 current  2024-10-14 18:08:29  24.11.20241009.5633bcf  6.6.54                          *

--- home.nix
+++ home.nix
@@ -92,21 +92,21 @@
     activitywatch = {
       enable = true;
       watchers = {
-       # aw-watcher-afk = {
-       #   package = pkgs.activitywatch;
-       #   settings = {
-       #     timeout = 300;
-       #     poll_time = 2;
-       #   };
-       # };
+        # aw-watcher-afk = {
+        #   package = pkgs.activitywatch;
+        #   settings = {
+        #     timeout = 300;
+        #     poll_time = 2;
+        #   };
+        # };
 
-       # aw-watcher-windows = {
-       #   package = pkgs.activitywatch;
-       #   settings = {
-       #     poll_time = 1;
-       #     exclude_title = true;
-       #   };
-       # };
+        # aw-watcher-windows = {
+        #   package = pkgs.activitywatch;
+        #   settings = {
+        #     poll_time = 1;
+        #     exclude_title = true;
+        #   };
+        # };
 
         awatcher = {
           package = pkgs.awatcher;
