a8c2a550f96802ddc81f6715fca1d4fe6bdf1d2c
ahi6 <60984726+ahi6@users.noreply.github.com>
2024-12-24 16:18:00 +0100

    179 current  2024-12-24 16:17:48  25.05.20241219.d70bd19  6.6.66                          *

--- hosts/ahinix/configuration.nix
+++ hosts/ahinix/configuration.nix
@@ -228,6 +228,28 @@
   # Disable fwupd, don't need it
   services.fwupd.enable = false;
 
+  # grafana testing environment
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_addr = "127.0.0.1";
+        http_port = 3000;
+        enforce_domain = true;
+        enable_gzip = true;
+        domain = "grafana.your.domain";
+
+        # Alternatively, if you want to server Grafana from a subpath:
+        # domain = "your.domain";
+        # root_url = "https://your.domain/grafana/";
+        # serve_from_sub_path = true;
+      };
+
+      # Prevents Grafana from phoning home
+      #analytics.reporting_enabled = false;
+    };
+  };
+
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
