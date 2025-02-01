41c6a3c22f535431739374ccce1e586bb577d158
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-02-01 18:32:10 +0100

    196 current  2025-02-01 18:32:05  25.05.20250129.9d3ae80  6.12.11                          *

--- modules/jackaudio.nix
+++ modules/jackaudio.nix
@@ -7,10 +7,10 @@
     jackd.enable = true;
     #    jackd.extraOptions = ["-dalsa" "--device" "hw:PCH,0"];
     # support ALSA only programs via ALSA JACK PCM plugin
-    alsa.enable = false;
+    alsa.enable = true;
     # support ALSA only programs via loopback device (supports programs like Steam)
     loopback = {
-      enable = true;
+      enable = false;
       # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
       #dmixConfig = ''
       #  period_size 2048
