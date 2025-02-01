29b2c80026b539708e78fc6e241f91eb09e3734d
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-02-01 18:30:00 +0100

    195 current  2025-02-01 18:29:55  25.05.20250129.9d3ae80  6.12.11                          *

--- modules/jackaudio.nix
+++ modules/jackaudio.nix
@@ -5,17 +5,17 @@
 }: {
   services.jack = {
     jackd.enable = true;
-    jackd.extraOptions = ["-dalsa" "--device" "hw:PCH,0"];
+    #    jackd.extraOptions = ["-dalsa" "--device" "hw:PCH,0"];
     # support ALSA only programs via ALSA JACK PCM plugin
     alsa.enable = false;
     # support ALSA only programs via loopback device (supports programs like Steam)
     loopback = {
-      enable = false;
+      enable = true;
       # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
       #dmixConfig = ''
       #  period_size 2048
       #'';
     };
   };
-  users.extraGroups.jackaudio.members = ["ahi"];
+  users.extraUsers.ahi.extraGroups = ["jackaudio"];
 }
