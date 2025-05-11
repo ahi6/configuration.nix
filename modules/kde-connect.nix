3003e0b7b67f6abd10bf93f076d5b147716a40cb
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-05-11 16:30:41 +0200

    Update flakes

--- modules/kde-connect.nix
+++ modules/kde-connect.nix
@@ -1,11 +1,7 @@
 {pkgs, ...}: {
-  #
+  # from https://wiki.nixos.org/wiki/KDE_Connect
   programs.kdeconnect = {
     enable = true;
     package = pkgs.gnomeExtensions.gsconnect;
   };
-  #networking.firewall = rec {
-  #  allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-  #  allowedUDPPortRanges = allowedTCPPortRanges;
-  #};
 }
