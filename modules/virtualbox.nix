255d2097215810dd58758b90858f2a375b5d8db3
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-07-12 18:16:11 +0200

    Update flakes

--- modules/virtualbox.nix
+++ modules/virtualbox.nix
@@ -4,5 +4,7 @@
   ...
 }: {
   virtualisation.virtualbox.host.enable = true;
+  # virtualisation.virtualbox.host.enableKvm = true;
+  # virtualisation.virtualbox.host.addNetworkInterface = false;
   users.extraGroups.vboxusers.members = ["ahi6"];
 }
