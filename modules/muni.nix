97f59b0da4a952797f0ef8b4aef80de1ca3b0264
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-09-25 23:30:13 +0200

    package edulint (todo: create NUR repo)
    edulint 3
    
    edulint 4
    
    move packages to packages

--- modules/muni.nix
+++ modules/muni.nix
@@ -3,17 +3,26 @@
   lib,
   ...
 }: let
+  python = pkgs.python313.override {
+    self = python;
+    packageOverrides = pyfinal: pyprev: {
+      edulint = pyfinal.callPackage ../packages/edulint.nix {};
+    };
+  };
+
   munipkgs = with pkgs; {
     pb015 = [
       haskell.compiler.ghcHEAD # aisa has version 9.2.1, but whatever
     ];
     ib111 = [
-      (python313.withPackages (python-pkgs:
+      (python.withPackages (python-pkgs:
         with python-pkgs; [
           tkinter
+          edulint
         ]))
       mypy
       ruff
+      thonny
     ];
   };
 in {
