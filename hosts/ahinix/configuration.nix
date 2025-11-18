edcd4fd47504e4a5afb569f649a78473e0f32512
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-11-18 13:06:37 +0100

    add more binary caches

--- hosts/ahinix/configuration.nix
+++ hosts/ahinix/configuration.nix
@@ -32,7 +32,19 @@
 
   networking.hostName = "ahinix"; # Define your hostname.
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    auto-optimise-store = true;
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://cache.garnix.io"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 
   # Enable networking
   networking.networkmanager.enable = true;
