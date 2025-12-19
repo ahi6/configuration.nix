cb97d943606a69d037938b26298568fd0e34a5a9
ahi6 <60984726+ahi6@users.noreply.github.com>
2025-12-19 12:56:13 +0100

    fish mommy

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,10 @@
 zoxide init fish | source
 nix-your-shell fish | source
 export PATH="/home/ahi/.local/bin:$PATH"
+
+function fish_right_prompt
+  set -l prevstatus $status
+  if string match -qr '(python|ghci)' -- $history[1]
+    mommy -1 -s $prevstatus
+  end
+end
