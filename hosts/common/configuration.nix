ed175433bf5e475ccded3a35869e1b2e5397c209
ahi6 <60984726+ahi6@users.noreply.github.com>
2026-06-05 19:04:00 +0200

    More font config

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -220,7 +220,18 @@
   fonts.packages = with pkgs; [
     corefonts
     monocraft
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
   ];
+  fonts.fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Serif" "Noto Serif CJK SC" "Noto Serif CJK JP" "Noto Serif CJK KR" ];
+        sansSerif = [ "Noto Sans" "Noto Sans CJK SC" "Noto Sans CJK JP" "Noto Sans CJK KR" ];
+        monospace = [ "JetBrains Mono" "Noto Sans Mono CJK SC" "Noto Sans Mono CJK JP" ];
+      };
+    };
   # Enable NixOS to use fonts from ~/.local/share/fonts
   fonts.fontDir.enable = true;
 
